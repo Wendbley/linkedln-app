2fea1705176abab23444d45a3b5405d512fe00c4
Johnny Wendbley <johnnywendbley@outlook.com>
2023-09-01 19:26:51 +0200

    Profile Page Completed

---
+++ app/components/profile/ProfileDesc.tsx
@@ -0,0 +1,24 @@
+import { Experiences } from '@/app/constants'
+import Image from 'next/image'
+import ProfileRow from './ProfileRow'
+import { rightArrow } from '@/public/images'
+import React from 'react'
+
+type Props = {}
+
+const ProfileDesc = (props: Props) => {
+	return (
+		<div className='profile-description'>
+			<h2>Experience</h2>
+			{Experiences.map((exp, index) => (
+				<ProfileRow key={index} {...exp} />
+			))}
+			<hr />
+			<a href='#' className='experience-link'>
+				SHOW ALL 7 experiences <Image src={rightArrow} alt='right-arrow-' />
+			</a>
+		</div>
+	)
+}
+
+export default ProfileDesc
